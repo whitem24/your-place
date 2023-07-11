4d3388a389bc8bb7f9c8c3d7bfa842e408b19cb0
Joe rodriguez <joeluisrr@gmail.com>
2023-07-11 17:08:38 -0400

    adding force-dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Image from 'next/image';
 import getCurrentUser from './actions/getCurrentUser';
 import getListings, { IListingsParams } from './actions/getListings';
 import ClientOnly from './components/ClientOnly';
 import Container from './components/Container';
 import EmptyState from './components/EmptyState';
 import ListingCard from './components/listings/ListingCard';
+
+export const dynamic = 'force-dynamic';
 
 interface HomeProps {
   searchParams: IListingsParams;
